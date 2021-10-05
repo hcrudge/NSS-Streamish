3680627c92057d00613f009c629521bff6ecdbb2
hcrudge <hcrudge1@gmail.com>
2021-10-05 13:26:18 -0500

    added get with comments functionality

--- Streamish/Startup.cs
+++ Streamish/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Streamish.Repositories;
 
 namespace Streamish
 {
@@ -26,7 +27,7 @@ namespace Streamish
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IVideoRepository, VideoRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
